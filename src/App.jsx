31bfec30c5c9f137fa3e3f16037359ddd97886bf
Dmitry <superflick3@gmail.com>
2025-04-02 12:35:10 +0300

    Testing styled components library

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,52 @@
+import styled from 'styled-components';
+import GlobalStyles from './styles/GlobalStyles';
+import Button from './ui/Button';
+import Input from './ui/Input';
+import Heading from './ui/Heading';
+import Row from './ui/Row';
+
+const StyledApp = styled.div`
+    background-color: orange;
+    padding: 20px;
+`;
+
 function App() {
-    return <div>Hi</div>;
+    return (
+        <>
+            <GlobalStyles />
+            <StyledApp>
+                <Row type="vertical">
+                    <Row type="horizontal">
+                        <Heading as="h1">The Oasis</Heading>
+                        <div>
+                            <Heading as="h2">Check in and out</Heading>
+                            <Button
+                                variation="primary"
+                                size="medium"
+                                onClick={() => alert('ASDASDAsd')}
+                            >
+                                ASDASDAS
+                            </Button>
+                            <Button
+                                variation="secondary"
+                                size="small"
+                                onClick={() => alert('WWWWW')}
+                            >
+                                ASDASDAS22222
+                            </Button>
+                        </div>
+                    </Row>
+                    <Row type="vertical">
+                        <Heading as="h3">Form</Heading>
+                        <form>
+                            <Input type="number" placeholder="number" />
+                            <Input type="text" placeholder="text" />
+                        </form>
+                    </Row>
+                </Row>
+            </StyledApp>
+        </>
+    );
 }
 
 export default App;
---
+++ src/ui/Input.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Input = styled.input`
+    border: 1px solit var(--color-grey-300);
+    border-radius: var(--border-radius-sm);
+    padding: 0.8rem 1.2rem;
+    background-color: var(--color-grey-200);
+`;
+
+export default Input;
