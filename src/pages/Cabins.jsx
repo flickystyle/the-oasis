856273c871d7b4526fef253e1ed8cf762c632381
Dmitry <superflick3@gmail.com>
2025-04-07 14:49:40 +0300

    Add alert(toast), delete cabin, some styling

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,25 +1,19 @@
-import { useEffect } from 'react';
+import { getCabins } from '../services/apiCabins';
 import Heading from '../ui/Heading';
 import Row from '../ui/Row';
-import { getCabins } from '../services/apiCabins';
+import CabinTable from '../features/cabins/CabinTable';
 
 function Cabins() {
-    useEffect(() => {
-        const f = async () => {
-            getCabins().then(console.log);
-        };
-        f();
-    }, []);
-
     return (
-        <Row type="horizontal">
-            <Heading as="h1">All cabins</Heading>
-            <p>TEST</p>
-            <img
-                src="https://flledracehzjzjggtmnk.supabase.co/storage/v1/object/public/cabin-images//cabin-001.jpg"
-                alt="cabin"
-            />
-        </Row>
+        <>
+            <Row type="horizontal">
+                <Heading as="h1">All cabins</Heading>
+                <p>Filter / Sort</p>
+            </Row>
+            <Row>
+                <CabinTable />
+            </Row>
+        </>
     );
 }
 
