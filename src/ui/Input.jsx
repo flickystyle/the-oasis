4a5d5a30e7e6d19ad74eb156ca5440ea51e1c724
Dmitry <superflick3@gmail.com>
2025-04-11 16:00:19 +0300

    Add settings and update logic

--- src/ui/Input.jsx
+++ src/ui/Input.jsx
@@ -4,7 +4,7 @@ const Input = styled.input`
     border: 1px solit var(--color-grey-300);
     border-radius: var(--border-radius-sm);
     padding: 0.8rem 1.2rem;
-    background-color: var(--color-grey-200);
+    background-color: var(--color-grey-100);
 `;
 
 export default Input;
--- src/ui/Row.jsx
+++ src/ui/Row.jsx
@@ -1,6 +1,6 @@
 import styled, { css } from 'styled-components';
 
-const Row = styled.div.attrs((props) => props || { type: 'vertical' })`
+const Row = styled.div`
     display: flex;
 
     ${(props) =>
