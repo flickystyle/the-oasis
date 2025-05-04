3d3e0f32c90b6bf5ce8449b58ad91ef278332988
Dmitry <superflick3@gmail.com>
2025-05-04 09:40:59 +0300

    Fix open/close menu bug in cabins

--- src/ui/Menus.jsx
+++ src/ui/Menus.jsx
@@ -85,6 +85,7 @@ function Toggle({ id }) {
     const { openId, open, close, setPosition } = useContext(MenusContext);
 
     function handleClick(e) {
+        e.stopPropagation();
         const rect = e.target.closest('button').getBoundingClientRect();
         setPosition({
             x: window.innerWidth - rect.width - rect.x,
@@ -100,7 +101,8 @@ function Toggle({ id }) {
 }
 function List({ id, children }) {
     const { openId, position, close } = useContext(MenusContext);
-    const ref = useOutsideClick(close);
+
+    const ref = useOutsideClick(close, false);
     if (id !== openId) return null;
 
     return createPortal(
